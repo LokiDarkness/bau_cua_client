e622cb365db32df21063ce608b8e78347bfb1e61
LokiDarkness <lokijuliver@gmail.com>
2026-01-10 20:46:40 +0700

    UPDATE DICE

--- src/components/DiceArea.jsx
+++ src/components/DiceArea.jsx
@@ -3,26 +3,39 @@ import { SLOT_ICONS } from '../constants/icons'
 export default function DiceArea({ rolling, dice }) {
   return (
     <div className="flex justify-center gap-4 mt-6 mb-4">
+
+      {/* ĐANG QUAY */}
       {rolling &&
-        [1,2,3].map(i => (
-          <div key={i}
+        [1, 2, 3].map(i => (
+          <div
+            key={i}
             className="w-16 h-16 bg-white rounded-2xl
                        flex items-center justify-center
-                       animate-dice">
+                       animate-dice"
+          >
             <span className="text-2xl font-bold">?</span>
           </div>
         ))
       }
 
+      {/* KẾT QUẢ */}
       {!rolling && dice &&
-        dice.map((d,i)=>(
-          <div key={i}
+        dice.map((d, i) => (
+          <div
+            key={i}
             className="w-16 h-16 bg-white rounded-2xl
-                       flex items-center justify-center">
-            <img src={SLOT_ICONS[d]} className="w-10 h-10" />
+                       flex items-center justify-center
+                       animate-dice-result"
+          >
+            <img
+              src={SLOT_ICONS[d]}
+              alt={d}
+              className="w-10 h-10"
+            />
           </div>
         ))
       }
+
     </div>
   )
 }
